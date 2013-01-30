d6e84a333b55c4df7c17c9189d44e4eb3e21fa58
Simon Højberg <r.hackr@gmail.com>
2013-01-30 00:51:27 -0500

    Change to be a grunt multi task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,18 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     yuiConfig: {
-      options: {
-        dest: 'yui_config.js',
-        groups: {
-          myGroup: {
-            comboBase: 'super/{{hash}}/path',
-            modules: ['test/fixtures/**/*.js'],
-            processPath: function (p) {
-              return p.replace('test', 'public');
-            },
-            excludeFiles: ['test/fixtures/not_me.js']
+      dev: {
+        options: {
+          dest: 'yui_config.js',
+          groups: {
+            myGroup: {
+              comboBase: 'super/{{hash}}/path',
+              modules: ['test/fixtures/**/*.js'],
+              processPath: function (p) {
+                return p.replace('test', 'public');
+              },
+              excludeFiles: ['test/fixtures/not_me.js']
+            }
           }
         }
       }
